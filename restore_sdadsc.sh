d1d41583288ac201bbdc123a8320a7d858cb31e5
Willem Vermeylen <18098873+FGOD1983@users.noreply.github.com>
2026-01-26 08:57:23 +0100

    changing Licence and removing unneeded folders

--- restore_sdadsc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Restore udev rules if they were removed by a SteamOS update
-if [ ! -f /etc/udev/rules.d/99-disable-steam-input.rules ]; then
-    echo "SteamOS update detected. Restoring udev rules..."
-    sudo cp "/home/deck/.local/share/scawp/SDADSC/99-disable-steam-input.rules.bak" /etc/udev/rules.d/99-disable-steam-input.rules
-    sudo udevadm control --reload
-fi
